6074ed2bafe8fad9b5a3a17da34b68880ba0ce55
tim Armstrong <tim_armstrong@sil.org>
2013-04-23 12:58:39 +0700

    Updated GeckoTestDialog to simulate inserting and removing a USB flash drive

--- src/SampleApp/GeckoTestDlg.cs
+++ src/SampleApp/GeckoTestDlg.cs
@@ -7,18 +7,28 @@ namespace SampleApp
 {
 	public partial class GeckoTestDlg : Form
 	{
+	    private readonly HtmlButton _usbButton ;
+	    private readonly HtmlButton _toggleInsertUsbFlashDrive;
+
 		public GeckoTestDlg()
 		{
 			InitializeComponent();
 			var browser = new GeckoWebBrowser {Dock = DockStyle.Fill};
 			browser.Navigate("file://" + Path.Combine(Environment.CurrentDirectory, "GeckoTestDlg.htm"));
 			Controls.Add(browser);
-			browser.DomClick += browser_DomClick;
+		    _usbButton = new HtmlButton(browser, "UsbButton");
+            _toggleInsertUsbFlashDrive = new HtmlButton(browser, "toggleInsertUsbFlashDrive");
+            _usbButton.Clicked += OnUsbClicked;
+		    _toggleInsertUsbFlashDrive.Clicked += OnToggleInsertUsbFlashDriveButtonClicked;
 		}
 
-		void browser_DomClick(object sender, GeckoDomEventArgs e)
-		{
-			throw new NotImplementedException();
-		}
+	    private void OnToggleInsertUsbFlashDriveButtonClicked(object sender, EventArgs e)
+	    {
+	        _usbButton.Enable = !_usbButton.Enable;
+	    }
+
+	    private void OnUsbClicked(object sender, EventArgs e)
+	    {
+	    }
 	}
 }
